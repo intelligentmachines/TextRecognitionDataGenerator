83796f9a3ba43668bf85e0a8c3a32295be2247af
ArowaYasmeen <arowa@intelligentmachin.es>
2021-12-05 13:36:37 +0600

    Setting default margin to 0 and including random number of words

--- trdg/run.py
+++ trdg/run.py
@@ -270,7 +270,7 @@ def parse_arguments():
         type=margins,
         nargs="?",
         help="Define the margins around the text when rendered. In pixels",
-        default=(5, 5, 5, 5),
+        default=(0, 0, 0, 0),
     )
     parser.add_argument(
         "-fi",
@@ -393,20 +393,20 @@ def main():
 
     # Creating synthetic sentences (or word)
     strings = []
-
+    length = rnd.randint(1,6)
     if args.use_wikipedia:
-        strings = create_strings_from_wikipedia(args.length, args.count, args.language)
+        strings = create_strings_from_wikipedia(length, args.count, args.language)
     elif args.input_file != "":
         strings1 = create_strings_from_file(args.input_file, args.count)
         for _ in range(0, args.count):
             current_string = ""
-            for _ in range(0, rnd.randint(1, args.length) if args.random else args.length):
+            for _ in range(0, rnd.randint(1, length) if args.random else length):
                 current_string += rnd.choice(strings1)
                 current_string += " "
             strings.append(current_string[:-1])
     elif args.random_sequences:
         strings = create_strings_randomly(
-            args.length,
+            length,
             args.random,
             args.count,
             args.include_letters,
@@ -423,7 +423,7 @@ def main():
             args.name_format = 2
     else:
         strings = create_strings_from_dict(
-            args.length, args.random, args.count, lang_dict
+            length, args.random, args.count, lang_dict
         )
 
     if args.language == "ar":
@@ -477,9 +477,9 @@ def main():
                 [args.word_split] * string_count,
                 [args.image_dir] * string_count,
                 [args.brightness_value] * string_count,
-                # [args.stroke_width] * string_count,
-                # [args.stroke_fill] * string_count,
-                # [args.image_mode] * string_count,
+                [args.stroke_width] * string_count,
+                [args.stroke_fill] * string_count,
+                [args.image_mode] * string_count,
             ),
         ),
         total=args.count,
